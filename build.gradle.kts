ca56496415c6d5508d7ac596a131586e7134d21b
Dmitry <103250419+Doutyn@users.noreply.github.com>
2025-01-22 14:52:47 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,12 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.23-1.0.19" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
+    id("org.sonarqube") version "6.0.1.5171"
+}
+    
+sonar {
+  properties {
+    property("sonar.projectKey", "ChaikaKotlin")
+    property("sonar.projectName", "ChaikaKotlin")
+  }
 }
