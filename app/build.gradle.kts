54814cd0daa396f58d6d2eae95fccc6072032279
OwletsFox <Aristark-Inferno@yandex.ru>
2025-02-26 21:09:02 +0300

    [CI] update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
     id("com.google.dagger.hilt.android")
-    // id("io.gitlab.arturbosch.detekt")
+    id("jacoco")
 }
 
 android {
@@ -13,7 +13,6 @@ android {
     defaultConfig {
         applicationId = "com.example.chaika"
         minSdk = 24
-        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "0.0.1-alpha"
@@ -33,7 +32,7 @@ android {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
+                "proguard-rules.pro"
             )
         }
     }
@@ -56,8 +55,50 @@ android {
     }
 }
 
-dependencies {
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
 
+jacoco {
+    toolVersion = "0.8.10" // Совместимо с SonarQube
+}
+
+tasks.register<JacocoReport>("jacocoTestReport") {
+    dependsOn("testDebugUnitTest")
+
+    reports {
+        xml.required.set(true)
+        xml.outputLocation.set(layout.buildDirectory.file("reports/jacoco/jacoco.xml"))
+        html.required.set(true)
+        csv.required.set(false)
+    }
+
+    val fileFilter = listOf(
+        "**/R.class",
+        "**/R$*.class",
+        "**/BuildConfig.*",
+        "**/Manifest*.*",
+        "**/*Test*.*",
+        "android/**/*.*"
+    )
+
+    val mainSrc = "$projectDir/src/main/java"
+
+    sourceDirectories.setFrom(files(mainSrc))
+    classDirectories.setFrom(
+        fileTree(layout.buildDirectory.dir("tmp/kotlin-classes/debug")) {
+            exclude(fileFilter)
+        }
+    )
+    executionData.setFrom(fileTree(layout.buildDirectory.dir("jacoco")) {
+        include(
+            "testDebugUnitTest.exec",
+            "outputs/unit_test_code_coverage/debugUnitTest/testDebugUnitTest.exec"
+        )
+    })
+}
+
+dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
@@ -87,10 +128,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-scalars:2.3.0")
 
     androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
-
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
     testImplementation("androidx.arch.core:core-testing:2.2.0")
     testImplementation("androidx.test:core:1.6.1")
     testImplementation("androidx.test:runner:1.6.2")
@@ -102,83 +141,38 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.3")
     testImplementation("org.junit.vintage:junit-vintage-engine:5.9.3")
 
-
-
     testImplementation("org.mockito:mockito-core:4.5.1")
     testImplementation("org.mockito:mockito-inline:4.0.0")
     testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
     testImplementation("org.mockito:mockito-junit-jupiter:4.0.0")
 
-
     val roomVersion = "2.6.1"
-
     implementation("androidx.room:room-runtime:$roomVersion")
-
     ksp("androidx.room:room-compiler:$roomVersion")
-
-    // To use Kotlin Symbol Processing (KSP)
-    ksp("androidx.room:room-compiler:2.6.1")
-
-    // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$roomVersion")
-
-    // optional - RxJava2 support for Room
-    implementation("androidx.room:room-rxjava2:$roomVersion")
-
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$roomVersion")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$roomVersion")
-
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$roomVersion")
-
-    // optional - Paging 3 Integration
     implementation("androidx.room:room-paging:$roomVersion")
 
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-
     implementation("androidx.activity:activity-ktx:1.8.2")
-    // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.0")
 
     val lifecycleVersion = "2.7.0"
-    // Lifecycle components
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
 
     // Hilt dependencies
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
-
     androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
     kspAndroidTest("com.google.dagger:hilt-compiler:2.48")
-
-    // Test dependencies for Hilt
     testImplementation("com.google.dagger:hilt-android-testing:2.48")
     kspTest("com.google.dagger:hilt-compiler:2.48")
     testImplementation("io.mockk:mockk:1.12.0")
-
-    // Детекторы кода
-    // implementation("com.pinterest:ktlint:0.50.0")
-    // detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.6")
-
-    tasks.withType<Test> {
-        useJUnitPlatform()
-    }
-    
 }
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
 }
-
-// ktlint {
-//    version.set("0.48.0")
-//    android.set(true)
-//    outputToConsole.set(true)
-//    enableExperimentalRules.set(true)
-// }
