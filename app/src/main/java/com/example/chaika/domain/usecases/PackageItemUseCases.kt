e1f014c580ab7398c4dd2be714b68e7c18d00223
OwletsFox <Aristark-Inferno@yandex.ru>
2025-04-02 22:36:43 +0300

    ackagetemsease

---
+++ app/src/main/java/com/example/chaika/domain/usecases/PackageItemUseCases.kt
@@ -0,0 +1,14 @@
+package com.example.chaika.domain.usecases
+
+import com.example.chaika.data.room.repo.RoomPackageItemRepositoryInterface
+import com.example.chaika.domain.models.PackageItemDomain
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GetPackageItemUseCase @Inject constructor(
+    private val repository: RoomPackageItemRepositoryInterface,
+) {
+    operator fun invoke(): Flow<List<PackageItemDomain>> {
+        return repository.getAllPackageItems()
+    }
+}
