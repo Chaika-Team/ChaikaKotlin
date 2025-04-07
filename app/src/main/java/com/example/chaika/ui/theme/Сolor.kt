a3dc18a7226a9e8bd9bdbd1585593630f4f362e1
DyukovNA <1ceptnpd@gmail.com>
2025-04-07 20:15:07 +0300

    CHK-41 Fixed comments

--- app/src/main/java/com/example/chaika/ui/theme/Сolor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chaika.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val RedRZD = Color(0xFFE21A1A)  // Цвет активной иконки
-val BlackInactive = Color(0x00000000) // Цвет неактивной иконки
-val BottomBarBackground = Color(0xFFFFFFFF) // Фон BottomBar
